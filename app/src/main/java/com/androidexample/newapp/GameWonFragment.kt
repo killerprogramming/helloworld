82ff001c9296d62cf21935a99d33ef5f48a7285e
Shraddha Moradiya <shraddhakothiya@gmail.com>
2020-04-05 22:24:04 +0200

    GameFragment Created

---
+++ app/src/main/java/com/androidexample/newapp/GameWonFragment.kt
@@ -0,0 +1,29 @@
+
+package com.androidexample.newapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.androidexample.newapp.databinding.FragmentGameWonBinding
+
+
+class GameWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_game_won, container, false)
+
+        binding.nextMatchButton.setOnClickListener { view -> view
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+        }
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+        return binding.root
+    }
+}
--- app/src/main/java/com/androidexample/newapp/MainActivity.kt
+++ app/src/main/java/com/androidexample/newapp/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.androidexample.newapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.androidexample.newapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController) //Link the NavController to our ActionBar.
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
